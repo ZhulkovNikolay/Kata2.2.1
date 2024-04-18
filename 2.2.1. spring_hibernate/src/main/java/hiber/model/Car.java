c664d84a4b3c4d225e9ef9f226c155444dac9489
ZhulkovNikolay <wed10@yandex.ru>
2024-04-18 20:10:03 +0300

    -В модель Car и User добавлены переопределения equals() и hashcode()

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -4,6 +4,7 @@ package hiber.model;
 //3. Добавьте этот класс в настройки hibernate.
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "cars")
@@ -49,6 +50,19 @@ public class Car {
 
     public void setSeries(int series) {
         this.series = series;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return series == car.series && Objects.equals(id, car.id) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model, series);
     }
 
     @Override
